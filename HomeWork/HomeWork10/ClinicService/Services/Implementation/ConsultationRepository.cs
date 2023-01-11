cb434f985bb5eb54a3341a0367e12138104fea51
Oleg Glumov <darek2587@gmail.com>
2023-01-11 23:39:25 +0300

    added sources task

---
+++ HomeWork/HomeWork10/ClinicService/Services/Implementation/ConsultationRepository.cs
@@ -0,0 +1,36 @@
+﻿using ClinicService.Models;
+
+namespace ClinicService.Services.Implementation
+{
+    /// <summary>
+    /// Домашняя работа: Доработать имплементацию для текущего репозитория
+    /// </summary>
+    public class ConsultationRepository : IConsultationRepository
+    {
+        private const string connectionString = "Data Source = clinic.db; Version = 3; Pooling = true; Max Pool Size = 100;";
+        public int Create(Consultation item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Consultation> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Consultation GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Consultation item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HomeWork/HomeWork10/ClinicService/Services/Implementation/PetRepository.cs
@@ -0,0 +1,36 @@
+﻿using ClinicService.Models;
+
+namespace ClinicService.Services.Implementation
+{
+    /// <summary>
+    /// Домашняя работа: Доработать имплементацию для текущего репозитория
+    /// </summary>
+    public class PetRepository : IPetRepository
+    {
+        private const string connectionString = "Data Source = clinic.db; Version = 3; Pooling = true; Max Pool Size = 100;";
+        public int Create(Pet item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Pet> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pet GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Pet item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
